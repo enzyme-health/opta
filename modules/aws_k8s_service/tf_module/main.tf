6f31709717d7e75f17f76ec3e9f6764bc52b3106
Jason Bush <jason.bush@wheel.com>
2025-02-26 12:59:29 -0600

    cleaned up commas.

--- modules/aws_k8s_service/tf_module/main.tf
+++ modules/aws_k8s_service/tf_module/main.tf
@@ -14,38 +14,38 @@ resource "helm_release" "k8s-service" {
     yamlencode({
       deployment_timestamp = timestamp()
       autoscaling = {
-        minReplicas = var.min_containers,
-        maxReplicas = var.max_containers,
+        minReplicas = var.min_containers
+        maxReplicas = var.max_containers
         targetCPUUtilizationPercentage = var.autoscaling_target_cpu_percentage
         targetMemoryUtilizationPercentage = var.autoscaling_target_mem_percentage
-      },
-      httpPort = var.http_port,
-      probePort = var.probe_port,
-      ports = var.ports,
-      serviceAnnotations = var.service_annotations,
+      }
+      httpPort = var.http_port
+      probePort = var.probe_port
+      ports = var.ports
+      serviceAnnotations = var.service_annotations
       containerResourceLimits = {
         memory = var.resource_limits == null ? "${var.resource_request["memory"] * 2}Mi" : "${var.resource_limits["memory"]}Mi"
-      },
+      }
       containerResourceRequests = {
         cpu = "${var.resource_request["cpu"]}m"
         memory = "${var.resource_request["memory"]}Mi"
-      },
-      deployPods = (local.uppercase_image != "AUTO") || (var.tag != null) || (var.digest != null),
+      }
+      deployPods = (local.uppercase_image != "AUTO") || (var.tag != null) || (var.digest != null)
       image = local.image
       version = var.tag == null ? "latest" = var.tag
-      livenessProbePath = var.healthcheck_path == null || var.liveness_probe_path != null ? var.liveness_probe_path : var.healthcheck_path,
-      livenessProbeCommand = length(var.healthcheck_command) == 0 || length(var.liveness_probe_command) != 0 ? var.liveness_probe_command : var.healthcheck_command,
+      livenessProbePath = var.healthcheck_path == null || var.liveness_probe_path != null ? var.liveness_probe_path : var.healthcheck_path
+      livenessProbeCommand = length(var.healthcheck_command) == 0 || length(var.liveness_probe_command) != 0 ? var.liveness_probe_command : var.healthcheck_command
       initialLivenessDelay = var.initial_liveness_delay
-      readinessProbePath = var.healthcheck_path == null || var.readiness_probe_path != null ? var.readiness_probe_path : var.healthcheck_path,
-      readinessProbeCommand = length(var.healthcheck_command) == 0 || length(var.readiness_probe_command) != 0 ? var.readiness_probe_command : var.healthcheck_command,
+      readinessProbePath = var.healthcheck_path == null || var.readiness_probe_path != null ? var.readiness_probe_path : var.healthcheck_path
+      readinessProbeCommand = length(var.healthcheck_command) == 0 || length(var.readiness_probe_command) != 0 ? var.readiness_probe_command : var.healthcheck_command
       initialReadinessDelay = var.initial_readiness_delay
-      healthcheck_path = var.healthcheck_path,
-      envVars = var.env_vars,
-      linkSecrets = var.link_secrets,
-      uriComponents = local.uri_components,
-      layerName = var.layer_name,
-      moduleName = var.module_name,
-      environmentName = var.env_name,
+      healthcheck_path = var.healthcheck_path
+      envVars = var.env_vars
+      linkSecrets = var.link_secrets
+      uriComponents = local.uri_components
+      layerName = var.layer_name
+      moduleName = var.module_name
+      environmentName = var.env_name
       iamRoleArn = aws_iam_role.k8s_service.arn
       stickySession = var.sticky_session
       stickySessionMaxAge = var.sticky_session_max_age
