6d6e65aefc1082e8693065b801e081c1c0f30d73
Rosalie James <rosalie.james@wheel.com>
2025-02-26 20:49:02 -0600

    remove commas but keep coalesce changes

--- modules/aws_k8s_service/tf_module/main.tf
+++ modules/aws_k8s_service/tf_module/main.tf
@@ -12,49 +12,49 @@ resource "helm_release" "k8s-service" {
   name  = "${var.layer_name}-${var.module_name}"
   values = [
     yamlencode({
-      deployment_timestamp = timestamp(),
+      deployment_timestamp = timestamp()
       autoscaling = {
         minReplicas = var.min_containers
         maxReplicas = var.max_containers
         targetCPUUtilizationPercentage = var.autoscaling_target_cpu_percentage
         targetMemoryUtilizationPercentage = var.autoscaling_target_mem_percentage
-      },
-      httpPort = var.http_port,
-      probePort = var.probe_port,
-      ports = var.ports,
-      serviceAnnotations = var.service_annotations,
+      }
+      httpPort = var.http_port
+      probePort = var.probe_port
+      ports = var.ports
+      serviceAnnotations = var.service_annotations
       containerResourceRequests = {
-        cpu    = var.resource_request["cpu"],
+        cpu    = var.resource_request["cpu"]
         memory = var.resource_request["memory"]
-      },
-      deployPods = (local.uppercase_image != "AUTO") || (var.tag != null) || (var.digest != null),
-      image = local.image,
-      version = var.tag == null ? "latest" : var.tag,
-      livenessProbePath = coalesce(var.liveness_probe_path, var.healthcheck_path),
-      livenessProbeCommand = coalesce(var.liveness_probe_command, var.healthcheck_command),
-      initialLivenessDelay = var.initial_liveness_delay,
-      readinessProbePath = coalesce(var.readiness_probe_path, var.healthcheck_path),
-      readinessProbeCommand = coalesce(var.readiness_probe_command, var.healthcheck_command),
-      initialReadinessDelay = var.initial_readiness_delay,
-      healthcheck_path = var.healthcheck_path,
-      envVars = var.env_vars,
-      linkSecrets = var.link_secrets,
-      uriComponents = local.uri_components,
-      layerName = var.layer_name,
-      moduleName = var.module_name,
-      environmentName = var.env_name,
-      iamRoleArn = aws_iam_role.k8s_service.arn,
-      stickySession = var.sticky_session,
-      stickySessionMaxAge = var.sticky_session_max_age,
-      consistentHash = var.consistent_hash,
-      keepPathPrefix = var.keep_path_prefix,
-      persistentStorage = var.persistent_storage,
-      ingressExtraAnnotations = var.ingress_extra_annotations,
-      tolerations = var.tolerations,
-      cron_jobs = var.cron_jobs,
-      podAnnotations = var.pod_annotations,
-      podLabels = var.pod_labels,
-      commands = var.commands,
+      }
+      deployPods = (local.uppercase_image != "AUTO") || (var.tag != null) || (var.digest != null)
+      image = local.image
+      version = var.tag == null ? "latest" : var.tag
+      livenessProbePath = coalesce(var.liveness_probe_path, var.healthcheck_path)
+      livenessProbeCommand = coalesce(var.liveness_probe_command, var.healthcheck_command)
+      initialLivenessDelay = var.initial_liveness_delay
+      readinessProbePath = coalesce(var.readiness_probe_path, var.healthcheck_path)
+      readinessProbeCommand = coalesce(var.readiness_probe_command, var.healthcheck_command)
+      initialReadinessDelay = var.initial_readiness_delay
+      healthcheck_path = var.healthcheck_path
+      envVars = var.env_vars
+      linkSecrets = var.link_secrets
+      uriComponents = local.uri_components
+      layerName = var.layer_name
+      moduleName = var.module_name
+      environmentName = var.env_name
+      iamRoleArn = aws_iam_role.k8s_service.arn
+      stickySession = var.sticky_session
+      stickySessionMaxAge = var.sticky_session_max_age
+      consistentHash = var.consistent_hash
+      keepPathPrefix = var.keep_path_prefix
+      persistentStorage = var.persistent_storage
+      ingressExtraAnnotations = var.ingress_extra_annotations
+      tolerations = var.tolerations
+      cron_jobs = var.cron_jobs
+      podAnnotations = var.pod_annotations
+      podLabels = var.pod_labels
+      commands = var.commands
       args = var.args
     })
   ]
