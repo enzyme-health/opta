16c5fb908df003f03c8accfe85d8c54929fe9054
nmagallon <nadia.magallon@wheel.com>
2025-03-07 13:28:53 -0700

    Update main.tf

--- modules/k8s_manifest/tf_module/main.tf
+++ modules/k8s_manifest/tf_module/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 1.13.3"
+      version = "1.14.0"  # Try upgrading to a later version
     }
   }
 }
